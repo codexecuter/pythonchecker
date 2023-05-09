d43621f5fa894e687bc88d972b23e8460b870829
Berk <62399309+codexecuter@users.noreply.github.com>
2023-05-09 12:59:37 -0700

    Update gsmtc.py

--- gsmtc.py
+++ gsmtc.py
@@ -1,36 +1,39 @@
 import mysql.connector
 
 # Veritabanı bağlantısı için gerekli bilgileri girin
+database = input("GSM Databaseinizin ismini girin (Ornek: hacerdedegsm, 120mgsm): ")
+frmname = input("GSM Databaseinizin frm ismini girin (Ornek: gsm.frm, 120mgsm.frm): ")
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="avea"
+    database= database
 )
 
 # Cursor oluştur
 mycursor = mydb.cursor()
 
-# Kullanıcıdan girdiyi al
 gsm = input("GSM giriniz: ")
 
 # Verileri veritabanında ara ve sonucu yazdır
 try:
-    # TC ile eşleşen kaydı seç
-    sql = f"SELECT * FROM gsm WHERE GSM = '{gsm}'"
+    # GSM ile eşleşen kaydı seç
+    sql = f"SELECT * FROM {frmname} WHERE GSM = '{gsm}'"
 
     # Sorguyu çalıştır
     mycursor.execute(sql)
 
-    # Sonuçları yazdır
-    result = mycursor.fetchone()
-    if result:
-        tc = result[0]
-        gsm = result[1]
-        print(f"TC: {tc}")
-        print(f"GSM: {gsm}")
+     # Tüm sonuçları yazdır
+    results = mycursor.fetchall()
+    if len(results) > 0:
+        for result in results:
+            tc = result[0]
+            gsm = result[1]
+            print(f"TC: {tc}")
+            print(f"GSM: {gsm}")
     else:
-        print("Eslesen veri yok.")
+        print("Eşleşen veri yok.")
     
 except mysql.connector.errors.ProgrammingError:
     print("Hata: Geçersiz sorgu.")
