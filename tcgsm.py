f71616de81fd618a01a417fd2c9308ddbea46b19
Berk <62399309+codexecuter@users.noreply.github.com>
2023-05-09 13:00:56 -0700

    Create tcgsm.py

---
+++ tcgsm.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+# Veritabanı bağlantısı için gerekli bilgileri girin
+database = input("GSM Databaseinizin ismini girin (Ornek: hacerdedegsm, 120mgsm): ")
+frmname = input("GSM Databaseinizin frm ismini girin (Ornek: gsm.frm, 120mgsm.frm): ")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database= database
+)
+
+# Cursor oluştur
+mycursor = mydb.cursor()
+
+tc = input("TC giriniz: ")
+
+# Verileri veritabanında ara ve sonucu yazdır
+try:
+    # GSM ile eşleşen kaydı seç
+    sql = f"SELECT * FROM {frmname} WHERE TC = '{tc}'"
+
+    # Sorguyu çalıştır
+    mycursor.execute(sql)
+
+     # Tüm sonuçları yazdır
+    results = mycursor.fetchall()
+    if len(results) > 0:
+        for result in results:
+            tc = result[0]
+            gsm = result[1]
+            print(f"GSM: {gsm}")
+    else:
+        print("Eşleşen veri yok.")
+    
+except mysql.connector.errors.ProgrammingError:
+    print("Hata: Geçersiz sorgu.")
+except Exception as e:
+    print("Hata:", e)
