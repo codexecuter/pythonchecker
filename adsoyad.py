a3ca22525b2fe48dd304817012ab0d4150b52860
Berk <62399309+codexecuter@users.noreply.github.com>
2023-11-18 13:03:07 +0300

    Update adsoyad.py

--- adsoyad.py
+++ adsoyad.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-# Veritabanı bağlantısı için gerekli bilgileri girin
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -8,19 +8,19 @@ mydb = mysql.connector.connect(
     database="101m"
 )
 
-# Cursor oluştur
+
 mycursor = mydb.cursor()
 
-# Kullanıcıdan girdileri al
+
 adi = input("Adı: ")
 soyadi = input("Soyadı: ")
 il = input("İl (Boş bırakabilirsiniz): ")
 nufusil = input("NufusIl (Boş bırakabilirsiniz): ")
 
-# Verilerin yazılacağı dosyanın adını oluştur
+
 dosya_adi = adi + "_" + soyadi + ".txt"
 
-# Verileri veritabanında ara ve dosyaya yaz
+
 try:
     if il:
         sql = f"SELECT * FROM 101m WHERE ADI = '{adi}' AND SOYADI = '{soyadi}' AND NUFUSIL = '{il}'"
@@ -31,10 +31,9 @@ try:
 
 
 
-    # Sorguyu çalıştır
     mycursor.execute(sql)
 
-    # Sonuçları dosyaya yaz
+
     with open(dosya_adi, "wb") as dosya:
         for kayit in mycursor:
             tc = kayit[1]
